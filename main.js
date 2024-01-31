7635578556a226d8f7fe131d810e2fc0edff76d5
AtaMLK <molkaraeeata@gmail.com>
2024-01-31 07:05:19 +0300

    m

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const day = document.querySelector(".day");
 const month = document.querySelector(".month");
 const year = document.querySelector(".year");
@@ -9,83 +8,62 @@ let calcedYear = document.querySelector(".calculated-year");
 let calcedMonth = document.querySelector(".calculated-month");
 let calcedDay = document.querySelector(".calculated-day");
 const now = new Date();
-let errorMessage;
+let notValidMessage;
+/* const date1 = now.getTime();
+const date2 = new Date("1989-03-02").getTime();
+const year1 = Math.floor((date1 - date2) / 31536000000);
+const month1 = Math.floor(((date1 - date2) / 2628288000) - Math.floor((date1 - date2) / 31536000000) * 12);
+const day1 =( Math.floor((date1 - date2)/(86400000)) - Math.floor(((year1*365)+month1*30.7)));
+console.log(((date1 - date2) / 31536000000) * 12);
+console.log(((date1 - date2) / 2628288000));
+console.log(year1, month1, day1);
 
-const showMessage = function (element, message) {
-    if(errorMessage){
-      errorMessage.remove()
-    }
+let notValidMessage; */
 
-      console.log(message)
-    errorMessage = document.createElement("p");
-    errorMessage.style.color = "var(--Lightred)";
-    errorMessage.style.fontSize = "0.6rem";
-    errorMessage.textContent = message;
-    element.insertAdjacentElement("afterend", errorMessage);
-    element.value = "";
+const validMessage = function (element, message) {
+  notValidMessage = document.createElement("p");
+  notValidMessage.style.color = "var(--Lightred)";
+  notValidMessage.style.fontSize = "0.6rem";
+  notValidMessage.textContent = message;
+  element.insertAdjacentElement("afterend", notValidMessage);
+  element.value = "";
 };
 
-const notValidEntry = function () {
+const validEntry = function () {
   if (day.value > 31 || day.value == "" || day.value <= 0) {
-    showMessage(day, "Not a valid Day");
+    validMessage(day, "Not a valid Day");
+    day.textContent = "";
   }
-  if ( month.value >= 13 || month.value == "" || month.value < 0) {
-    showMessage(month, "Not a valid Month");
+  if (month.value >= 13 || month.value == "" || month.value <= 0) {
+    validMessage(month, "Not a valid Month");
   }
   if (year.value > now.getFullYear() || year.value == "" || year.value <= 0) {
-    showMessage(year, "Not a valid year");
+    validMessage(year, "Not a valid year");
   }
+  return;
 };
 
 const calcAge = function () {
-  // Check if any of the required input fields are empty
-  if (day.value === "" || month.value === "" || year.value === "" ) {
-    notValidEntry()
-    // Display an error message or handle the case as needed
+  if (!validEntry) {
     return;
+  } else {
+    button.addEventListener("click", function () {
+      let thisYear = +(now.getFullYear() - year.value);
+      let thisMonth = +(now.getMonth() + 1 - month.value);
+      let thisDay = +(now.getDate() - day.value);
+      if (month.value > now.getMonth() + 1) thisYear--;
+      else {
+        if (month.value == now.getMonth() + 1) {
+          if (day.value > now.getDate()) thisYear--;
+        }
+      }
+
+      calcedYear.textContent = thisYear;
+      calcedMonth.textContent = thisMonth >= 0 ? thisMonth : 12+thisMonth;
+      calcedDay.textContent = thisDay > 0 ? thisDay : -thisDay;
+      console.log(thisYear);
+      return thisYear;
+    });
   }
-    let thisMonth = +(now.getMonth() - month.value )  + 1;
-    let thisDay = +(now.getDate() - day.value);
-    let thisYear = +(now.getFullYear() - year.value) -1
-
-    if (now.getFullYear() > year.value ){calcedYear.textContent = thisYear;
-    }
-    else if(now.getFullYear()=year.value) {
-      
-      calcedYear.textContent = "00"
-    }
-    else{
-      
-      calcedYear.textContent = "--"
-    }
-    if ( (now.getMonth()+1) >= month.value){
-      calcedMonth.textContent = thisMonth
-    }
-    else if ( 12 < month.value){
-      calcedMonth.textContent = "--"
-    }
-    else{
-      calcedMonth.textContent = 13 - month.value
-    }
-    if (now.getDate() >= day.value ){
-      calcedDay.textContent = thisDay
-    }
-    else if (day.value > 31 ){
-      
-      calcedDay.textContent = "--"
-    }
-    else{
-      calcedDay.textContent =30 + thisDay
-    }
-    if(now.getDate() <= day.value && (now.getMonth()+1) <= month.value  ){
-      calcedYear.textContent = +(now.getFullYear() - year.value);
-    }
-  notValidEntry()
-   // Clear input values after calculation
-   day.value = "";
-   month.value = "";
-   year.value = "";
 };
-
-// Trigger calcAge() when the button is clicked
-button.addEventListener("click", calcAge);
+calcAge();
